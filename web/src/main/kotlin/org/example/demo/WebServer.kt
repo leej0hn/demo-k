6752cc286b8849b795dbaabec98a40f21cc36e94
lidx83 <lidx83@midea.com>
2024-11-20 14:09:23 +0800

    test Thread.join

--- web/src/main/kotlin/org/example/demo/WebServer.kt
+++ web/src/main/kotlin/org/example/demo/WebServer.kt
@@ -1,6 +1,5 @@
 package org.example.demo
 
-import org.mybatis.spring.annotation.MapperScan
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
--- web/src/main/kotlin/org/example/demo/controller/TestController.kt
+++ web/src/main/kotlin/org/example/demo/controller/TestController.kt
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.Executors
+
 
 @RestController
 class TestController {
@@ -28,12 +30,50 @@ class TestController {
     }
 
     @GetMapping("/test")
-    fun testV(): String {
-        Thread.sleep(400)
+    fun test(): String {
+        logger.info("test---start..")
+        val sleep1 = sleep1()
+        val sleep2 = sleep2()
+        val sleep3 = sleep3()
+        logger.info("test---join start..")
+        sleep1.join()
+        logger.info("test---join1..")
+        sleep2.join()
+        logger.info("test---join2..")
+        sleep3.join()
+        logger.info("test---join3..")
+        logger.info("test---end..")
         return """
             {
             "success":true
             }
         """.trimIndent();
+    }
+
+    fun sleep1(): Thread {
+        Executors.newVirtualThreadPerTaskExecutor().submit{
+
+        }
+        return Thread.startVirtualThread {
+            logger.info("sleep1---start..")
+            Thread.sleep(400)
+            logger.info("sleep1---end..")
+        }
+    }
+
+    fun sleep2(): Thread {
+        return Thread.startVirtualThread {
+            logger.info("sleep2---start..")
+            Thread.sleep(400)
+            logger.info("sleep2---end..")
+        }
+    }
+
+    fun sleep3(): Thread {
+        return Thread.startVirtualThread {
+            logger.info("sleep3---start..")
+            Thread.sleep(400)
+            logger.info("sleep3---end..")
+        }
     }
 }
